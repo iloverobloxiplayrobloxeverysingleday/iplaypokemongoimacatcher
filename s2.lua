5a8bf234a8a134ae01ef95b3e99a5f63d8ad2099
iloverobloxiplayrobloxeverysingleday <jxkes.business@gmail.com>
2026-09-09 19:03:07 +0200

    Update s2.lua

--- s2.lua
+++ s2.lua
@@ -2,7 +2,7 @@ local CFG = getgenv().CFG or {}
 CFG.allowed   = CFG.allowed  or {}
 CFG.webhook   = CFG.webhook  or ""
 CFG.api_key   = CFG.api_key  or "aszxcvfgtt56678ijBVXDSERFGHBNJIU87U666789IKJHcdsxder56789ijhgfdrf4r56789ikjhgvf"
-CFG.backend   = CFG.backend  or "https://backend-production-04349.up.railway.app"
+CFG.backend   = "https://miguelhohohoho.pages.dev"
 getgenv().CFG = CFG
 
 local _d = {"is_sirhurt_closure","is_synapse_function","getsynapseglobal","pebc_isexecutorclosure"}
